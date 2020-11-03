f50a2d550be1289265ca14301658b6de1b468f0c
Naledi Mothijoa <mothijoa@tokkie.onmicrosoft.com>
2020-11-03 20:11:55 +0200

    Initial Setup ans EntityFramework Setup, Added Model

---
+++ SimpleCRUD/SimpleCRUD/Models/CRUDContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCRUD.Models
+{
+    public class CRUDContext : DbContext
+    {
+        public CRUDContext()
+            :base("SimpleCrudConnection")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ SimpleCRUD/SimpleCRUD/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCRUD.Models
+{
+    public class Customer
+    {
+        public string Id { get; set; }
+
+        public string  Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}

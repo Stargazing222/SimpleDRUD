18023a2761a8c2b6bd27a871e534badbc6012300
Naledi Mothijoa <mothijoa@tokkie.onmicrosoft.com>
2020-11-04 11:45:48 +0200

    Added Migration

---
+++ SimpleCRUD/SimpleCRUD/Migrations/202011040941376_Initial.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleCRUD.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Email = c.String(),
+                        Phone = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
